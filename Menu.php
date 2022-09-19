94ee57c5e86d5558c910f3dcd756194555a06872
Michael Ganchrow <92171498+Shmuel28@users.noreply.github.com>
2022-09-19 09:10:06 -0400

    Create Header, Menu, and Footer

---
+++ Menu.php
@@ -0,0 +1,6 @@
+<ul id="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Class Registration</a></li>
+            <li><a href="#">College Outlook</a></li>
+            <li><a href="#">To-Do</a></li>
+</ul>
